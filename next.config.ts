7910f0314e0f10413d8f05865b8ad2a4717467c2
Ian Chong <ianczm@live.co.uk>
2025-01-01 16:46:31 +0800

    Setup: Add Payload.

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,5 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -6,4 +8,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
