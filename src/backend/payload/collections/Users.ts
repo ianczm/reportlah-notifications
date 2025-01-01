fcd7858ccda7926149e113c7d7d5f230786a0258
Ian Chong <ianczm@live.co.uk>
2025-01-01 18:30:49 +0800

    Chore: Add Payload migrations.

--- src/backend/payload/collections/Users.ts
+++ src/backend/payload/collections/Users.ts
@@ -1,13 +1,31 @@
-import type { CollectionConfig } from 'payload'
+import { v4 as uuid } from "uuid";
+
+import type { CollectionConfig } from "payload";
 
 export const Users: CollectionConfig = {
-  slug: 'users',
+  slug: "users",
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: "email",
   },
   auth: true,
+  hooks: {
+    beforeValidate: [
+      ({ data, operation }) => {
+        if (operation === "create") {
+          if (!data?.id) {
+            return { ...data, id: uuid() };
+          }
+        }
+        return { data };
+      },
+    ],
+  },
   fields: [
+    {
+      name: "id",
+      type: "text",
+    },
     // Email added by default
     // Add more fields as needed
   ],
-}
+};
